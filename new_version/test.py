46b2d88ba2928c7ba13e2e76a13b97e68e4aab1c
ilyashirko <ilyashirko@gmail.com>
2022-02-08 13:21:19 +0300

    deleted extra file

--- new_version/test.py
+++
@@ -1,6 +0,0 @@
-genres = [{"id": 18, "name": "драма"},{"id": 80,"name": "криминал"},{"id": 35,"name": "комедия"}]
-genres2 = [{"id": 18, "name": "драма"},{"id": 380,"name": "криминал"},{"id": 35,"name": "комедия"}]
-
-genres = set(genre["id"] for genre in genres)
-genres = genres & set(genre["id"] for genre in genres2)
-print(len(genres))
